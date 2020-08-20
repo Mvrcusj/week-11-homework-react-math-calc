e91086a8c1d3c6863308f5f95b86c4cbb97f497a
mvrcusj <mvrcusj@gmail.com>
2020-08-19 21:43:33 -0400

    Show working calculator with four functions

---
+++ week-11-homework-react-math-calc/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator'
+
+function App() {
+  return (
+    <div className="app calculator" >
+      <header>
+      React Calculator
+      </header>
+
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
